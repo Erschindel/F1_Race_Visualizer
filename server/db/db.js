4fb715e39c38981254494da171aaa139d19d41e4
Ethan Schindel <erschindel@gmail.com>
2022-05-02 14:48:05 -0400

    more heroku config attempts 2

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,28 @@
 const Sequelize = require("sequelize");
+const pkg = require("../../package.json");
+
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
+
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING === "true") {
+  delete config.logging;
+}
+
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/F1",
-  {
-    logging: false,
-  }
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
 );
 
 module.exports = db;
