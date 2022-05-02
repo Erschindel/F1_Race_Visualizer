945975e4c556fd97916f51d50bc79df2fad17c73
Ethan Schindel <erschindel@gmail.com>
2022-05-02 16:27:36 -0400

    heroku removed redux logging, deployed!

--- client/store.js
+++ client/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
-import raceReducer from './store/allRaces';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import raceReducer from "./store/allRaces";
+import thunkMiddleware from "redux-thunk";
+// import { createLogger } from "redux-logger";
 
 const store = createStore(
   raceReducer,
-  applyMiddleware(thunkMiddleware, createLogger())
+  applyMiddleware(thunkMiddleware) // , createLogger()
 );
 
 export default store;
