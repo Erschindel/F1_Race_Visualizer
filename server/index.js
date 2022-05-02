bec996277f255b9b0090295d23e6238faf550d41
Ethan Schindel <erschindel@gmail.com>
2022-05-02 14:41:15 -0400

    more heroku config attempts

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
-const db = require('./db/db.js');
-const app = require('./app');
-const port = process.env.PORT || 3000;
+const db = require("./db/db.js");
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
 
-db.sync() // sync our database
-  .then(function () {
-    app.listen(port, () => console.log(`running on port ${port}`));
-  });
+const seed = require("./db/seed.js");
+
+const init = async () => {
+  try {
+    if (process.env.SEED === "true") {
+      await seed();
+    } else {
+      await db.sync();
+    }
+    // start listening (and create a 'server' object representing our server)
+    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+init();
+
+// db.sync() // sync our database
+//   .then(function () {
+//     app.listen(port, () => console.log(`running on port ${port}`));
+//   });
