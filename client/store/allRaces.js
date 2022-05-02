b9747d98409e42f1c704c5a39cb3f29c013d8d40
Ethan Schindel <erschindel@gmail.com>
2022-05-02 16:19:52 -0400

    heroku fixing requests

--- client/store/allRaces.js
+++ client/store/allRaces.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const GET_RACES_FROM_YEAR = 'GET_RACES_FROM_YEAR';
-const GET_MOST_RECENT = 'GET_MOST_RECENT';
-const GET_LAPS = 'GET_LAPS';
+const GET_RACES_FROM_YEAR = "GET_RACES_FROM_YEAR";
+const GET_MOST_RECENT = "GET_MOST_RECENT";
+const GET_LAPS = "GET_LAPS";
 
 const getAllFromYear = (races) => {
   return {
@@ -29,7 +29,7 @@ export const getLatestRace = () => {
   return async (dispatch) => {
     try {
       const external = await axios.get(
-        'http://ergast.com/api/f1/current/last/results.json'
+        "https://ergast.com/api/f1/current/last/results.json"
       );
       dispatch(getMostRecent(external.data.MRData.RaceTable.Races[0]));
     } catch (error) {
@@ -41,7 +41,9 @@ export const getLatestRace = () => {
 export const getAllRaces = (year) => {
   return async (dispatch) => {
     try {
-      const external = await axios.get(`http://ergast.com/api/f1/${year}.json`);
+      const external = await axios.get(
+        `https://ergast.com/api/f1/${year}.json`
+      );
       dispatch(getAllFromYear(external.data.MRData.RaceTable.Races));
     } catch (error) {
       console.log(error);
@@ -55,9 +57,9 @@ export const getRaceLaps = (year, round, name, lap) => {
       // get data from ergast
       // if that works, get data from my server
       const external = await axios.get(
-        `http://ergast.com/api/f1/${year}/${round}/laps/${lap}.json`
+        `https://ergast.com/api/f1/${year}/${round}/laps/${lap}.json`
       );
-      const slug = name.replace(/\s/g, '_');
+      const slug = name.replace(/\s/g, "_");
       const internal = await axios.get(`/api/tracks/${slug}`);
       external.data.MRData.RaceTable.Races[0]
         ? dispatch(
@@ -83,7 +85,7 @@ const initialState = {
   latest: {},
   laps: {
     race: {},
-    shape: '',
+    shape: "",
   },
 };
 
