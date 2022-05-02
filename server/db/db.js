58709973a1181ada59d017aa0d597dde78eaf465
Ethan Schindel <erschindel@gmail.com>
2022-05-02 13:46:50 -0400

    attempting heroku config 2

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(
-  process.env.DATABASE || "postgres://localhost:5432/F1",
+  process.env.DATABASE_URL || "postgres://localhost:5432/F1",
   {
     logging: false,
   }
