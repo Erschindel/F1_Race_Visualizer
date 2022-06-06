e3606d681a2bca1a4951f569d3dd3af34f84f66f
Ethan Schindel <erschindel@gmail.com>
2022-06-05 21:51:20 -0400

    Features: circuit info, GitHub link, emojis

--- server/api/tracks.js
+++ server/api/tracks.js
@@ -1,7 +1,38 @@
-const router = require('express').Router();
-const Track = require('../db/models/Track');
+const router = require("express").Router();
+const axios = require("axios");
+const cheerio = require("cheerio");
+const Track = require("../db/models/Track");
 
-router.get('/:slug', async function (req, res, next) {
+router.get("/wiki/:slug", async function (req, res, next) {
+  try {
+    const { data } = await axios.get(
+      `https://en.wikipedia.org/wiki/${req.params.slug}`
+    );
+    const $ = cheerio.load(data);
+    const wikiData = {};
+    const relevantRows = ["Circuit length", "Race length", "Laps"];
+
+    $(".infobox tbody") // parsing HTML with cheerio
+      .children()
+      .each((_, el) => {
+        $(el)
+          .children()
+          .each((_, innerEl) => {
+            const currentTr = $(innerEl).text();
+            if (relevantRows.includes(currentTr)) {
+              const nextItem = $(innerEl).next().text();
+              wikiData[currentTr] = nextItem;
+            }
+          });
+      });
+
+    res.status(200).send(wikiData);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:slug", async function (req, res, next) {
   try {
     const track = await Track.findOne({
       where: {
@@ -14,9 +45,9 @@ router.get('/:slug', async function (req, res, next) {
     next(error);
   }
 });
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   try {
-    res.send('tracks');
+    res.send("tracks");
   } catch (error) {
     next(error);
   }
